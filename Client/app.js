be925d2e6e184b1283e64017f7716530a8979805
xaipo <xaipoj@hotmail.com>
2015-11-24 12:04:40 -0500

    agrego estructura y navegacion con bugs del cliente

--- Client/app.js
+++ Client/app.js
@@ -7,6 +7,7 @@ var app = angular.module("myApp", ['ngRoute'])
    //$locationProvider.html5Mode(true);
     $routeProvider.when("/indexAdmin",{templateUrl:"/tesisSaludOcupacional/Client/Administrator/inicio.html"});
     $routeProvider.when("/newEmpresa",{templateUrl:"/tesisSaludOcupacional/Client/Administrator/newEmpresa.html", controller:'EmpresaController'});
+   // $routeProvider.when("/login",{templateUrl:"/tesisSaludOcupacional/Client/login.html", controller:'LoginController'});
 });
 
 app.controller('EmpresaController', ['$scope', '$http', function ($scope, $http) {
@@ -16,6 +17,35 @@ app.controller('EmpresaController', ['$scope', '$http', function ($scope, $http)
 
 }]);
 
+
+app.controller('LoginController', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.mensaje = "msj";
+    $scope.usuario = "";
+    $scope.password = "";
+
+    $scope.login = function () {
+        $scope.mensaje = "procesando";
+        console.log("funciona");
+        console.log($scope.usuario);
+        console.log($scope.password);
+        /*$http.get('http://localhost:26077/api/SincronizarBases').
+         success(function (data, status, headers, config) {
+         if (data == "ok") {
+         $scope.mensaje = "Sincronizadas correctamente";
+         } else {
+         $scope.mensaje = "Error el servidor por seguridad bloquea peticiones seguidas intente más tarde";
+         }
+         }).
+         error(function (data, status, headers, config) {
+         $scope.mensaje = "no conecta al servidor";
+         });*/
+
+    };
+
+
+
+}]);
 
     /*app.config(['$routeProvider', function ($routeProvider) {
 
--- Client/controllers/LoginController.js
+++ Client/controllers/LoginController.js
@@ -4,31 +4,3 @@
 
 
 
-app.controller('LoginController', ['$scope', '$http', function ($scope, $http) {
-
-    $scope.mensaje = "msj";
-    $scope.usuario = "";
-    $scope.password = "";
-
-    $scope.login = function () {
-        $scope.mensaje = "procesando";
-        console.log("funciona");
-        console.log($scope.usuario);
-        console.log($scope.password);
-        /*$http.get('http://localhost:26077/api/SincronizarBases').
-            success(function (data, status, headers, config) {
-                if (data == "ok") {
-         $scope.mensaje = "Sincronizadas correctamente";
-         } else {
-                    $scope.mensaje = "Error el servidor por seguridad bloquea peticiones seguidas intente más tarde";
-                }
-            }).
-            error(function (data, status, headers, config) {
-                $scope.mensaje = "no conecta al servidor";
-            });*/
-
-    };
-
-
-
-}]);
