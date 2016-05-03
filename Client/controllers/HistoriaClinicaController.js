544523e902ac83a0fc2c12a28ea7beaed2007001
edycriminal89 <edisonfgarcia@hotamil.com>
2016-05-02 21:30:22 -0500

    termino primera ventana historia clinica

--- Client/controllers/HistoriaClinicaController.js
+++ Client/controllers/HistoriaClinicaController.js
@@ -9,21 +9,399 @@ app.controller('HistoriaClinicaController', ['$scope', '$http', '$location','myP
     $scope.mensaje = "";
     $scope.PacienteEncontrado="";
     $scope.pacienteBusqueda="";
+    $scope.fechaExamen;
+    $scope.tipoHistoria=[];
+    $scope.tipoHistoriaSelected="";
 
+    $scope.historiaClinica=  {
+
+        paciente:"",
+        fecha_examen:"",
+        tipo_examen:""
+
+    }
 
     //realizar el control si tiene una historia clinica vigente
 
+    // <editor-fold defaultstate="collapsed" desc="Tipo Historia CLinica">
+
+
+        $http({
+
+            method: 'GET',
+            url: myProvider.getTipoHistoria1(),
+
+            headers: {
+                'Content-Type': 'application/json'
+            }
+
+        }).then(function successCallback(response) {
+            //console.log('entra url');
+            //console.log(url);
+
+            var n = response.data.length;
+            // console.log(n);
+
+            if(n==0){
+
+                alert('no se encontro el paciente');
+
+            }else {
+                $scope.tipoHistoria=[];
+                for(var i=0;i<n;i++){
+
+                    $scope.tipoHistoria.push(response.data[i]);
+
+                }
+
+
+            }
+
+
+        }, function errorCallback(response) {
+            console.log('entra');
+            //  Console.log(response);
+            $scope.mesaje = response.mensaje;
+
+        });
+
+
+
+    // </editor-fold>
+
+
+    // <editor-fold defaultstate="collapsed" desc="Busqueda Materia Prima">
+
+    function  getMateriaPrima(array) {
+        $scope.pacienteEncontrado.puesto_trabajo.materia_prima=[]
+        for(var aux in array) {
+            $http({
+                method: 'GET',
+                url: myProvider.getMateriaPrima() + '?_id' +aux,
+
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+
+            }).then(function successCallback(response) {
+                // console.log('url');
+
+                var n = response.data.length;
+                // console.log(n);
+
+                if (n == 0) {
+
+                    alert('no se encontro el paciente');
+
+                } else {
+
+
+                    $scope.pacienteEncontrado.puesto_trabajo.materia_prima.push(response.data[0]);
+
+
+                }
+
+
+            }, function errorCallback(response) {
+
+                //  Console.log(response);
+                $scope.mesaje = response.mensaje;
+
+            });
+          //  console.log($scope.pacienteEncontrado.puesto_trabajo.materia_prima);
+        }
+
+
+    }
+
+    // </editor-fold>
 
 
 
 
 
+    // <editor-fold defaultstate="collapsed" desc="Busqueda Herramientas">
 
+    function  getHerramientas(array) {
+        $scope.pacienteEncontrado.puesto_trabajo.herramienta=[]
+        for(var aux in array) {
+            $http({
+                method: 'GET',
+                url: myProvider.getHerramientas() + '?_id' +aux,
+
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+
+            }).then(function successCallback(response) {
+                // console.log('url');
+
+                var n = response.data.length;
+                // console.log(n);
+
+                if (n == 0) {
+
+                    alert('no se encontro el paciente');
+
+                } else {
+
+
+                        $scope.pacienteEncontrado.puesto_trabajo.herramienta.push(response.data[0]);
+                }
+
+
+            }, function errorCallback(response) {
+
+                //  Console.log(response);
+                $scope.mesaje = response.mensaje;
+
+            });
+        }
+
+    }
+
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Busqueda Ciudad">
+
+    function  getCiudad(id) {
+        $http({
+            method: 'GET',
+            url: myProvider.getCiudad()+"?_id="+id,
+
+            headers: {
+                'Content-Type': 'application/json'
+            }
+
+        }).then(function successCallback(response) {
+
+
+            var n = response.data.length;
+
+
+            if (n == 0) {
+
+                alert('no se encontro el paciente');
+
+            } else {
+                $scope.pacienteEncontrado.ciudad = response.data[0];
+
+
+            }
+
+
+        }, function errorCallback(response) {
+
+
+            $scope.mesaje = response.mensaje;
+
+        });
+
+    }
+
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Busqueda Nivel Estudios">
+
+    function  getNivelEstudios(id) {
+        $http({
+            method: 'GET',
+            url: myProvider.getNivelEstudio()+"?_id="+id,
+
+            headers: {
+                'Content-Type': 'application/json'
+            }
+
+        }).then(function successCallback(response) {
+
+
+            var n = response.data.length;
+
+
+            if (n == 0) {
+
+                alert('no se encontro el paciente');
+
+            } else {
+                $scope.pacienteEncontrado.nivel_estudio = response.data[0];
+                 //.log($scope.pacienteEncontrado.nivel_estudio);
+
+
+            }
+
+
+        }, function errorCallback(response) {
+
+
+            $scope.mesaje = response.mensaje;
+
+        });
+
+    }
+
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Busqueda Estado civil">
+
+    function  getEstadoCivil(id) {
+        $http({
+            method: 'GET',
+            url: myProvider.getEstadoCivil()+"?_id="+id,
+
+            headers: {
+                'Content-Type': 'application/json'
+            }
+
+        }).then(function successCallback(response) {
+
+
+            var n = response.data.length;
+
+
+            if (n == 0) {
+
+                alert('no se encontro el paciente');
+
+            } else {
+                $scope.pacienteEncontrado.estado_civil = response.data[0];
+
+            }
+
+
+        }, function errorCallback(response) {
+
+
+            $scope.mesaje = response.mensaje;
+
+        });
+
+    }
+
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Busqueda Empresa">
+
+    function  getEmpresa(id) {
+        $http({
+            method: 'GET',
+            url: myProvider.getEmpresa()+"?_id="+id,
+
+            headers: {
+                'Content-Type': 'application/json'
+            }
+
+        }).then(function successCallback(response) {
+
+
+            var n = response.data.length;
+
+
+            if (n == 0) {
+
+                alert('no se encontro el paciente');
+
+            } else {
+                $scope.pacienteEncontrado.puesto_trabajo.dependencia.empresa = response.data[0];
+
+
+            }
+
+
+        }, function errorCallback(response) {
+
+
+            $scope.mesaje = response.mensaje;
+
+        });
+
+    }
+
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Busqueda Puesto Trabajo">
+    function  getPuestoTrabajo(id){
+
+        $http({
+            method: 'GET',
+            url: myProvider.getPuestoTrabajo()+"?_id="+id+"&&estado=1",
+
+            headers: {
+                'Content-Type': 'application/json'
+            }
+
+        }).then(function successCallback(response) {
+           // console.log('url');
+
+            var n = response.data.length;
+            // console.log(n);
+
+            if(n==0){
+
+                alert('no se encontro el paciente');
+
+            }else {
+                $scope.pacienteEncontrado.puesto_trabajo = response.data[0];
+
+                getDependencia($scope.pacienteEncontrado.puesto_trabajo.dependencia);
+                getHerramientas($scope.pacienteEncontrado.puesto_trabajo.herramienta);
+                getMateriaPrima($scope.pacienteEncontrado.puesto_trabajo.materia_prima);
+            }
+
+
+        }, function errorCallback(response) {
+
+            //  Console.log(response);
+            $scope.mesaje = response.mensaje;
+
+        });
+
+
+    }
+             // </editor-fold>
+
+
+    // <editor-fold defaultstate="collapsed" desc=" busqueda Dependencia">
+    function  getDependencia(id){
+        //console.log('entra dependencia');
+       // console.log(myProvider.getDependencia()+"?_id="+id);
+        $http({
+            method: 'GET',
+            url: myProvider.getDependencia()+"?_id="+id,
+
+            headers: {
+                'Content-Type': 'application/json'
+            }
+
+        }).then(function successCallback(response) {
+
+
+            var n = response.data.length;
+            // console.log(n);
+
+            if(n==0){
+
+                alert('no se encontro el paciente');
+
+            }else {
+                $scope.pacienteEncontrado.puesto_trabajo.dependencia = response.data[0];
+              //  console.log($scope.pacienteEncontrado.puesto_trabajo.dependencia.id_empresa);
+                getEmpresa($scope.pacienteEncontrado.puesto_trabajo.dependencia.id_empresa)
+
+            }
+
+
+        }, function errorCallback(response) {
+
+            //  Console.log(response);
+            $scope.mesaje = response.mensaje;
+
+        });
+
+
+
+
+    }
+        // </editor-fold>
     // <editor-fold defaultstate="collapsed" desc="Busqueda Paciente">
     $scope.searchPaciente=function(){
         $scope.mensaje = "procesando";
         if($scope.pacienteBusqueda!="") {
-              console.log(myProvider.getPaciente()+"?cedula="+$scope.pacienteBusqueda);
+            //  console.log(myProvider.getPaciente()+"?cedula="+$scope.pacienteBusqueda);
                 $http({
                     method: 'GET',
                     url: myProvider.getPaciente()+"?cedula="+$scope.pacienteBusqueda,
@@ -33,7 +411,7 @@ app.controller('HistoriaClinicaController', ['$scope', '$http', '$location','myP
                     }
 
                 }).then(function successCallback(response) {
-                    console.log('url');
+                    //console.log('url');
 
                    var n = response.data.length;
                   //  console.log(n);
@@ -48,48 +426,12 @@ app.controller('HistoriaClinicaController', ['$scope', '$http', '$location','myP
                        // console.log(aux);
                         $scope.pacienteEncontrado = (aux);
 
+                        getPuestoTrabajo($scope.pacienteEncontrado.puesto_trabajo);
+                        getEstadoCivil($scope.pacienteEncontrado.estado_civil);
+                        getCiudad($scope.pacienteEncontrado.ciudad);
+                        getNivelEstudios($scope.pacienteEncontrado.nivel_estudio);
 
 
-                        // <editor-fold defaultstate="collapsed" desc="Obtener descripciones Puesto Trabajo">
-                        $http({
-                            method: 'GET',
-                            url: myProvider.getPuestoTrabajo()+"?_id="+$scope.pacienteEncontrado.puesto_trabajo+"&&estado=1",
-
-                            headers: {
-                                'Content-Type': 'application/json'
-                            }
-
-                        }).then(function successCallback(response) {
-                            console.log('url');
-
-                            var n = response.data.length;
-                            // console.log(n);
-
-                            if(n==0){
-
-                                alert('no se encontro el paciente');
-
-                            }else {
-                                $scope.pacienteEncontrado.puesto_trabajo = response.data[0];
-                             //   console.log($scope.pacienteEncontrado.puesto_trabajo);
-
-
-                            }
-
-
-                        }, function errorCallback(response) {
-
-                            //  Console.log(response);
-                            $scope.mesaje = response.mensaje;
-
-                        });
-
-
-
-
-                        // </editor-fold>
-
-                        console.log($scope.pacienteEncontrado);
                     }
 
 
@@ -113,103 +455,34 @@ app.controller('HistoriaClinicaController', ['$scope', '$http', '$location','myP
 
 
 
-
-    /*
-// <editor-fold defaultstate="collapsed" desc="Inicializar">
-
-    $http({
-        method: 'GET',
-        url: 'http://localhost:3000/api/empresa',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-
-    }).then(function successCallback(response) {
-        // this callback will be called asynchronously
-        // when the response is available
-        // console.log(response);
-        var n=response.data.length;
-        //   console.log(response.data.length);
-        for(var i=0; i<n; i++){
-            var aux=response.data[i];
-            // console.log(aux);
-            $scope.listaEmpresas.push(aux);
-
-            // console.log($scope.listaEmpresas);
-
-        }
-
-
-    }, function errorCallback(response) {
-
-        Console.log(response);
-        $scope.mesaje=response.mensaje;
-
-    });
-// </editor-fold >
-
-    */
-
-/*
-    $scope.test = function () {
-        $scope.mensaje = "procesando";
-
-        console.log('entro');
-        $scope.empresaSelected=JSON.parse($scope.empresaSelected);
-        console.log($scope.empresaSelected.nombre_empresa);
-
-    };
+  //  window.localStorage.setItem("usuario", JSON.stringify($scope.usuario1));
 
 
 
-
-    $scope.searchDependenciaPorEmpresa = function () {
-
-        $scope.mensaje = "procesando";
-
-        if($scope.empresaSelected.length>0) {
-            $scope.listaDependencias=[];
-            console.log($scope.empresaSelected);
-            var contact = JSON.parse($scope.empresaSelected);
-
-            console.log(contact);
-            $http({
-                method: 'GET',
-                url: 'http://localhost:3000/api/dependencia?id_empresa=' + contact._id,
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-
-            }).then(function successCallback(response) {
-
-                $scope.depenendica = angular.fromJson(response.data[0]);
+    $scope.next1= function(){
 
 
-                console.log(response);
-                var n = response.data.length;
-                console.log(response.data.length);
-                for (var i = 0; i < n; i++) {
-                    var aux = response.data[i];
-                    console.log(aux);
-                    $scope.listaDependencias.push(aux);
+        if($scope.historiaClinica.tipo_examen!="" && $scope.pacienteEncontrado!=""&&$scope.historiaClinica.fecha_examen!="") {
+            alert("entra");
+            var dd=$scope.historiaClinica.fecha_examen;
 
-                    console.log($scope.listaDependencias);
+            console.log(dd);
 
-                }
+            //$scope.historiaClinica.fecha_examen=vec[0];
+            $scope.historiaClinica.paciente = $scope.pacienteEncontrado.id;
+            window.localStorage.setItem("hC", JSON.stringify($scope.historiaClinica));
 
-            }, function errorCallback(response) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-                Console.log(response);
-                $scope.mesaje = response.mensaje;
-
-            });
         }else{
 
-            $scope.mensaje="Seleccione una Empresa para cargar las dependencias";
+            alert("para continuar ingreseel paciente y  el tipo de historia clinica a elaborar");
         }
-    };
-*/
+    }
 
+
+    function transformDate(month){
+
+
+
+    }
 
 }]);
