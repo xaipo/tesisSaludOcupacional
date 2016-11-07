5bf4b4a6ee3db50ec56381cad1de917ed1708db3
xaipo <xaipoj@hotmail.com>
2016-11-06 22:53:04 -0500

    puta llave

--- Client/controllers/ControllerModificarAccidentesTrabajo.js
+++ Client/controllers/ControllerModificarAccidentesTrabajo.js
@@ -40,8 +40,9 @@ app.controller('ControllerModificarAccidentes', ['$scope', '$http', '$location',
                     //  $scope.tipoCie10=[];
                     for (var i = 0; i < n; i++) {
 
-
-                        $scope.listaAccidentesTrabajo.push(response.data[i]);
+                        var aux=response.data[i]
+                        aux.nombre_empresa={'nombre_empresa':aux.nombre_empresa};
+                        $scope.listaAccidentesTrabajo.push(aux);
                         // console.log($scope.tipoCie10);
 
                     }
@@ -96,4 +97,263 @@ app.controller('ControllerModificarAccidentes', ['$scope', '$http', '$location',
 
 
 
+
+  //  $scope.listaAccidentesTrabajo=[];
+    $scope.listaDetalleFactorRiesgo=[];
+    $scope.accidentesTrabajo={
+        codigo:"",
+        fecha_ocurrencia:"" ,
+        nombre_empresa:"",
+        naturaleza_lesion:"",
+        parte_cuerpo_afectada:"",
+        dias_incapacidad:"",
+        secuelas:""
+    };
+    $scope.accidentesTrabajoSelected="";
+
+    $scope.contador=0;
+    $scope.mapAccidentesTrabajo= new Map();
+
+    $scope.empresas;
+
+    $scope.getHistoria= function(){
+
+        $scope.historiaClinica=JSON.parse(window.localStorage.getItem('hC'));
+        console.log($scope.historiaClinica);
+
+    }
+
+
+    $scope.agregar= function (){
+
+
+        $scope.accidentesTrabajo.fecha_ocurrencia= document.getElementById(('datepicker')).value;
+        $scope.accidentesTrabajo.nombre_empresa= JSON.parse($scope.accidentesTrabajo.nombre_empresa);
+        $scope.accidentesTrabajo.codigo=$scope.contador;
+        $scope.contador++;
+
+        $http({
+            method: 'POST',
+            url: myProvider.getAccidentesTrabajo(),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: {
+
+                fecha_ocurrencia : $scope.accidentesTrabajo.fecha_ocurrencia,
+                nombre_empresa:  $scope.accidentesTrabajo.nombre_empresa.nombre_empresa,
+                naturaleza_lesion:  $scope.accidentesTrabajo.naturaleza_lesion,
+                parte_cuerpo_afectada: $scope.accidentesTrabajo.parte_cuerpo_afectada,
+                dias_incapcidad: $scope.accidentesTrabajo.dias_incapcidad,
+                secuelas:  $scope.accidentesTrabajo.secuelas
+            }
+
+
+        }).then(function successCallback(response) {
+            //console.log(response.data);
+          //  $scope.listaAccidentesTrabajo.push($scope.accidentesTrabajo);
+            $scope.historiaClinica.accidentesTrabajo.push(response.data._id);
+
+
+                $http({
+                    method: 'PUT',
+                    url: myProvider.getHistoriaClinica() + '/' + $scope.historiaClinica._id,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    data: {
+                        accidentesTrabajo:  $scope.historiaClinica.accidentesTrabajo
+
+                    }
+
+
+                }).then(function successCallback(response) {
+
+                    console.log(response.data);
+                    window.localStorage.setItem("hm", JSON.stringify(response.data));
+                    // alert('seleccione que desea agregar a la historia clinica');
+                    //localStorage.removeItem('hci');
+                    //localStorage.removeItem('hC');
+                    //window.location = '/tesisSaludOcupacional/Client/Administrator/indexAdmin.html';
+
+
+                }, function errorCallback(response) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    // console.log(response);
+                    //$scope.mesaje = response.mensaje;
+
+                });
+
+
+
+
+
+
+            window.localStorage.setItem("hci", JSON.stringify($scope.historiaClinicaIngreso));
+            // console.log($scope.historiaClinicaIngreso.accidentesTrabajo);
+
+
+
+
+
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            // console.log(response);
+            //$scope.mesaje = response.mensaje;
+
+        });
+
+        //$scope.mensaje = "Para ingresar debe llenar el nombre de la empresa";
+
+        //  }
+
+
+
+
+
+
+
+
+
+
+
+        console.log($scope.listaAccidentesTrabajo);
+        //$scope.mapAccidentesTrabajo.set($scope.accidentesTrabajo.codigo,$scope.accidentesTrabajo);
+        // console.log($scope.mapAccidentesTrabajo.entries());
+        $scope.accidentesTrabajo={
+            codigo:"",
+            fecha_ocurrencia:"" ,
+            nombre_empresa:"",
+            naturaleza_lesion:"",
+            parte_cuerpo_afectada:"",
+            dias_incapacidad:"",
+            secuelas:""
+        };
+        //console.log($scope.mapAccidentesTrabajo);
+
+
+        $scope.iniciar();
+
+    }
+
+
+    $scope.quitar= function (){
+        console.log($scope.accidentesTrabajoSelected);
+        var n=  $scope.listaAccidentesTrabajo.length;
+        console.log(n);
+        var pos;
+        for(var i=0;i<n;i++ ){
+
+            if($scope.listaAccidentesTrabajo[i].codigo==$scope.accidentesTrabajoSelected.codigo){
+                console.log(i);
+                console.log($scope.accidentesTrabajoSelected.codigo);
+                console.log($scope.listaAccidentesTrabajo[i].codigo);
+
+                pos=i;
+                break;
+            }
+        }
+        console.log(pos);
+        $scope.listaAccidentesTrabajo.splice(pos,1);
+        //  console.log($scope.listaRiesgosOcupacionales);
+        //  console.log($scope.contador);
+    }
+
+
+    // <editor-fold defaultstate="collapsed" desc="Cargar empresa">
+    $http({
+
+        method: 'GET',
+        url: myProvider.getEmpresa(),
+
+        headers: {
+            'Content-Type': 'application/json'
+        }
+
+    }).then(function successCallback(response) {
+        //console.log('entra url');
+        //console.log(url);
+
+        var n = response.data.length;
+        // console.log(n);
+
+        if(n==0){
+
+            alert('no se encontro provincias');
+
+        }else {
+            $scope.empresas=[];
+
+            for(var i=0;i<n;i++){
+
+                $scope.empresas.push(response.data[i]);
+
+                // console.log($scope.empresas);
+            }
+            $scope.empresaSeleccionada=$scope.empresas[0]._id;
+            //console.log($scope.empresaSeleccionada);
+            // console.log($scope.empresas);
+
+
+        }
+
+
+    }, function errorCallback(response) {
+        console.log('entra');
+        //  Console.log(response);
+        $scope.mesaje = response.mensaje;
+
+    });
+
+    // </editor-fold >
+
+
+
+    $scope.setClickedRow = function(index,item){  //function that sets the value of selectedRow to current index
+
+        console.log('entra');
+        $scope.selectedRow = index;
+        $scope.accidentesTrabajoSelected=item;
+        console.log(item);
+        console.log($scope.accidentesTrabajoSelected);
+
+        /*console.log($scope.selectedRow);
+         console.log(item);*/
+    }
+
+
+    $scope.saveThird= function (){
+
+
+        $scope.historiaClinica.accidentesTrabajo=$scope.listaAccidentesTrabajo;
+        window.localStorage.setItem("hC", JSON.stringify($scope.historiaClinica));
+        window.location ='/tesisSaludOcupacional/Client/Administrator/HistoriaClinica/third.html';
+
+
+
+    }
+    $scope.skipt2= function (){
+
+
+        $scope.accidentesTrabajo={
+            codigo:"",
+            fecha_ocurrencia:"" ,
+            nombre_empresa:"",
+            naturaleza_lesion:"no aplica",
+            parte_cuerpo_afectada:"",
+            dias_incapacidad:"",
+            secuelas:""
+        };
+
+        $scope.listaAccidentesTrabajo=[];
+        $scope.listaAccidentesTrabajo.push($scope.accidentesTrabajo);
+        $scope.historiaClinica.accidentesTrabajo=$scope.listaAccidentesTrabajo;
+        window.localStorage.setItem("hC", JSON.stringify($scope.historiaClinica));
+        window.location ='/tesisSaludOcupacional/Client/Administrator/HistoriaClinica/third.html';
+
+
+
+    }
 } ]);
--- Client/controllers/validacionHistoriaClinica.js
+++ Client/controllers/validacionHistoriaClinica.js
@@ -11,7 +11,6 @@ app.controller('ControllerValidacionHistoria', ['$scope', '$http', '$location', 
     $scope.urlSeleccionada='';
 
     $scope.searchUser = function () {
-
         console.log(myProvider.getUser()+'?cedula='+$scope.cedula);
         $http({
 
@@ -23,8 +22,7 @@ app.controller('ControllerValidacionHistoria', ['$scope', '$http', '$location', 
             }
 
         }).then(function successCallback(response) {
-            //console.log('entra url');
-            //console.log(url);
+
 
             var n = response.data.length;
             // console.log(n);
@@ -35,24 +33,137 @@ app.controller('ControllerValidacionHistoria', ['$scope', '$http', '$location', 
 
             } else {
                 //  $scope.tipoCie10=[];
-               // for (var i = 0; i < n; i++) {
+                // for (var i = 0; i < n; i++) {
 
 
-                    $scope.pacienteEncontrado=response.data[0];
-                  //  window.localStorage.setItem("pe", JSON.stringify($scope.pacienteEncontrado));
-                     console.log($scope.pacienteEncontrado);
+                $scope.pacienteEncontrado = response.data[0];
+                //  window.localStorage.setItem("pe", JSON.stringify($scope.pacienteEncontrado));
+                console.log($scope.pacienteEncontrado);
 
 
-                var n=$scope.pacienteEncontrado.historias_clinicas.length;
+                var n = $scope.pacienteEncontrado.historias_clinicas.length;
 
                 if(n==0){
                     alert('necesita abrir la historia clinica por primera vez');
-                }else{
-                    if(n==1){
+                }else {
+
+
+                    if (n == 1) {
                         $http({
 
                             method: 'GET',
-                            url: myProvider.getHistoriaClinica()+'?_id='+$scope.pacienteEncontrado.historias_clinicas[0],
+                            url: myProvider.getHistoriaClinica() + '?_id=' + $scope.pacienteEncontrado.historias_clinicas[0],
+
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+
+                        }).then(function successCallback(response) {
+                            //console.log('entra url');
+                            //console.log(url);
+                            $scope.historiaEncontrada = response.data[0];
+                            $scope.historiaEncontrada.estado = '2';
+                            $http({
+                                method: 'POST',
+                                url: myProvider.getHistoriaClinica(),
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                data: {
+
+                                    tipo_examen: '571bbe7825df3fa80c7be754',
+                                    fecha_examen: new Date(),
+                                    riesgos_ocupacionales: $scope.historiaEncontrada.riesgos_ocupacionales,
+                                    accidentesTrabajo: $scope.historiaEncontrada.accidentesTrabajo,
+                                    gineco_obstetra: $scope.historiaEncontrada.gineco_obstetra,
+                                    ausentismo: $scope.historiaEncontrada.ausentismo,
+                                    enfermedades_actuales_historicas: $scope.historiaEncontrada.enfermedades_actuales_historicas,
+                                    antescedentes_familiares: $scope.historiaEncontrada.antescedentes_familiares,
+                                    antescedentes_personales: $scope.historiaEncontrada.antescedentes_personales,
+                                    inmunizacion: $scope.historiaEncontrada.inmunizacion,
+                                    habitos_toxicos: $scope.historiaEncontrada.habitos_toxicos,
+                                    organos_sistemas: $scope.historiaEncontrada.organos_sistemas,
+                                    examenes_laboratorio: $scope.historiaEncontrada.examenes_laboratorio,
+                                    examenes_paraclinicos: $scope.historiaEncontrada.examenes_paraclinicos,
+                                    examen_fisico: $scope.historiaEncontrada.examen_fisico,
+                                    diagnostico_ocupacional: $scope.historiaEncontrada.diagnostico_ocupacional,
+                                    diagnostico_noOcupacioanl: $scope.historiaEncontrada.diagnostico_noOcupacioanl,
+                                    concepto: $scope.historiaEncontrada.concepto,
+                                    restricciones_limitaciones: $scope.historiaEncontrada.restricciones_limitaciones,
+                                    recomendaciones: $scope.historiaEncontrada.recomendaciones,
+                                    remision_especialista: $scope.historiaEncontrada.remision_especialista,
+                                    nombre_especialista: $scope.historiaEncontrada.nombre_especialista,
+                                    reubicacion: $scope.historiaEncontrada.reubicacion,
+                                    estado: '2',
+                                }
+
+
+                            }).then(function successCallback(response) {
+                                //console.log(response.data);
+                                // console.log(response.data._id);
+                                window.localStorage.setItem("hm", JSON.stringify(response.data));
+                                // console.log($scope.historiaClinicaIngreso.gineco_obstetra);
+                                // window.localStorage.setItem("pe", JSON.stringify(response.data[0]));
+
+                                $scope.pacienteEncontrado.historias_clinicas.push(response.data._id);
+
+                                $http({
+                                    method: 'PUT',
+                                    url: myProvider.getPaciente() + '/' + $scope.pacienteEncontrado._id,
+                                    headers: {
+                                        'Content-Type': 'application/json'
+                                    },
+                                    data: {
+                                        historias_clinicas: $scope.pacienteEncontrado.historias_clinicas,
+
+                                    }
+
+
+                                }).then(function successCallback(response) {
+
+                                    console.log(response.data);
+                                    window.localStorage.setItem("pe", JSON.stringify(response.data));
+                                    alert('seleccione que desea agregar a la historia clinica');
+                                    //localStorage.removeItem('hci');
+                                    //localStorage.removeItem('hC');
+                                    //window.location = '/tesisSaludOcupacional/Client/Administrator/indexAdmin.html';
+
+
+                                }, function errorCallback(response) {
+                                    // called asynchronously if an error occurs
+                                    // or server returns response with an error status.
+                                    // console.log(response);
+                                    //$scope.mesaje = response.mensaje;
+
+                                });
+
+
+                            }, function errorCallback(response) {
+                                // called asynchronously if an error occurs
+                                // or server returns response with an error status.
+                                // console.log(response);
+                                //$scope.mesaje = response.mensaje;
+
+                            });
+
+
+
+
+
+                            // console.log(n);
+                        }, function errorCallback(response) {
+                            console.log('entra');
+                            //  Console.log(response);
+                            $scope.mesaje = response.mensaje;
+
+                        });
+                    }else{
+
+                        console.log('sino');
+                        $http({
+
+                            method: 'GET',
+                            url: myProvider.getHistoriaClinica() + '?_id=' + $scope.pacienteEncontrado.historias_clinicas[1],
 
                             headers: {
                                 'Content-Type': 'application/json'
@@ -65,154 +176,21 @@ app.controller('ControllerValidacionHistoria', ['$scope', '$http', '$location', 
                             var n = response.data.length;
                             // console.log(n);
 
-                            if (n == 0) {
 
-                                alert('no se encontro el paciente');
+                            //  $scope.tipoCie10=[];
+                            // for (var i = 0; i < n; i++) {
 
-                            } else
-                            if(n==1){
-                                //  $scope.tipoCie10=[];
-                                // for (var i = 0; i < n; i++) {
+                            console.log(response.data);
+                            $scope.historiaClinica = response.data;
+                            window.localStorage.setItem("pe", JSON.stringify($scope.pacienteEncontrado));
+                            window.localStorage.setItem("hm", JSON.stringify($scope.historiaClinica));
+                            // console.log($scope.tipoCie10);
 
-
-                                $scope.historiaEncontrada=response.data[0];
-                                $scope.historiaEncontrada.estado='2';
-                                $http({
-                                    method: 'POST',
-                                    url: myProvider.getHistoriaClinica(),
-                                    headers: {
-                                        'Content-Type': 'application/json'
-                                    },
-                                    data: {
-
-                                        tipo_examen:'571bbe7825df3fa80c7be754',
-                                        fecha_examen:new Date(),
-                                        riesgos_ocupacionales:$scope.historiaEncontrada.riesgos_ocupacionales,
-                                        accidentesTrabajo:$scope.historiaEncontrada.accidentesTrabajo,
-                                        gineco_obstetra:$scope.historiaEncontrada.gineco_obstetra,
-                                        ausentismo:$scope.historiaEncontrada.ausentismo,
-                                        enfermedades_actuales_historicas:$scope.historiaEncontrada.enfermedades_actuales_historicas,
-                                        antescedentes_familiares:$scope.historiaEncontrada.antescedentes_familiares,
-                                        antescedentes_personales:$scope.historiaEncontrada.antescedentes_personales,
-                                        inmunizacion:$scope.historiaEncontrada.inmunizacion,
-                                        habitos_toxicos:$scope.historiaEncontrada.habitos_toxicos,
-                                        organos_sistemas:$scope.historiaEncontrada.organos_sistemas,
-                                        examenes_laboratorio:$scope.historiaEncontrada.examenes_laboratorio,
-                                        examenes_paraclinicos:$scope.historiaEncontrada.examenes_paraclinicos,
-                                        examen_fisico:$scope.historiaEncontrada.examen_fisico,
-                                        diagnostico_ocupacional:$scope.historiaEncontrada.diagnostico_ocupacional,
-                                        diagnostico_noOcupacioanl:$scope.historiaEncontrada.diagnostico_noOcupacioanl,
-                                        concepto:$scope.historiaEncontrada.concepto,
-                                        restricciones_limitaciones:$scope.historiaEncontrada.restricciones_limitaciones,
-                                        recomendaciones:$scope.historiaEncontrada.recomendaciones,
-                                        remision_especialista:$scope.historiaEncontrada.remision_especialista,
-                                        nombre_especialista:$scope.historiaEncontrada.nombre_especialista,
-                                        reubicacion:$scope.historiaEncontrada.reubicacion,
-                                        estado:'2',
-                                    }
+                            // }
+                            //$scope.cie10Selected=response.data[0];
+                            // $scope.busquedaCie10Tipo();
 
 
-                                }).then(function successCallback(response) {
-                                    //console.log(response.data);
-                                    // console.log(response.data._id);
-                                     window.localStorage.setItem("hm", JSON.stringify(response.data));
-                                    // console.log($scope.historiaClinicaIngreso.gineco_obstetra);
-                                    // window.localStorage.setItem("pe", JSON.stringify(response.data[0]));
-
-                                    $scope.pacienteEncontrado.historias_clinicas.push(response.data._id);
-
-                                    $http({
-                                        method: 'PUT',
-                                        url: myProvider.getPaciente() + '/' + $scope.pacienteEncontrado._id,
-                                        headers: {
-                                            'Content-Type': 'application/json'
-                                        },
-                                        data: {
-                                            historias_clinicas:  $scope.pacienteEncontrado.historias_clinicas,
-
-                                        }
-
-
-                                    }).then(function successCallback(response) {
-
-                                        console.log(response.data);
-                                        window.localStorage.setItem("pe", JSON.stringify(response.data));
-                                        alert('seleccione que desea agregar a la historia clinica');
-                                        //localStorage.removeItem('hci');
-                                        //localStorage.removeItem('hC');
-                                        //window.location = '/tesisSaludOcupacional/Client/Administrator/indexAdmin.html';
-
-
-                                    }, function errorCallback(response) {
-                                        // called asynchronously if an error occurs
-                                        // or server returns response with an error status.
-                                        // console.log(response);
-                                        //$scope.mesaje = response.mensaje;
-
-                                    });
-
-
-
-                                }, function errorCallback(response) {
-                                    // called asynchronously if an error occurs
-                                    // or server returns response with an error status.
-                                    // console.log(response);
-                                    //$scope.mesaje = response.mensaje;
-
-                                });
-
-
-                                // console.log($scope.tipoCie10);
-
-                                // }
-                                //$scope.cie10Selected=response.data[0];
-                                // $scope.busquedaCie10Tipo();
-
-                            }else{
-                                $http({
-
-                                    method: 'GET',
-                                    url: myProvider.getHistoriaClinica()+'?_id='+$scope.pacienteEncontrado.historias_clinicas[1],
-
-                                    headers: {
-                                        'Content-Type': 'application/json'
-                                    }
-
-                                }).then(function successCallback(response) {
-                                    //console.log('entra url');
-                                    //console.log(url);
-
-                                    var n = response.data.length;
-                                    // console.log(n);
-
-                                    if (n == 0) {
-
-                                        alert('no se encontro el paciente');
-
-                                    } else {
-                                        //  $scope.tipoCie10=[];
-                                        // for (var i = 0; i < n; i++) {
-
-
-                                        $scope.historiaClinica=response.data[0];
-                                        window.localStorage.setItem("pe", JSON.stringify($scope.pacienteEncontrado));
-                                        window.localStorage.setItem("hm", JSON.stringify($scope.historiaClinica));
-                                        // console.log($scope.tipoCie10);
-
-                                        // }
-                                        //$scope.cie10Selected=response.data[0];
-                                        // $scope.busquedaCie10Tipo();
-
-                                    }
-
-
-                                }, function errorCallback(response) {
-                                    console.log('entra');
-                                    //  Console.log(response);
-                                    $scope.mesaje = response.mensaje;
-
-                                });
-                            }
 
 
                         }, function errorCallback(response) {
@@ -221,15 +199,17 @@ app.controller('ControllerValidacionHistoria', ['$scope', '$http', '$location', 
                             $scope.mesaje = response.mensaje;
 
                         });
+
+
+
                     }
 
                 }
-               // }
-                //$scope.cie10Selected=response.data[0];
-                // $scope.busquedaCie10Tipo();
+
+
+
 
             }
-
 
         }, function errorCallback(response) {
             console.log('entra');
@@ -237,6 +217,7 @@ app.controller('ControllerValidacionHistoria', ['$scope', '$http', '$location', 
             $scope.mesaje = response.mensaje;
 
         });
+
 
       //  $scope.validateFirst();
     }
